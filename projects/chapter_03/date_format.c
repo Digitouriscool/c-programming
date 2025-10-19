e44a738789521b2077470017fb9d4c4cbf40ae1f
Digitouriscool <36806477+Digitouriscool@users.noreply.github.com>
2025-10-19 15:39:40 -0600

    upload exercises

--- projects/chapter_03/date_format.c
+++ projects/chapter_03/date_format.c
@@ -10,7 +10,7 @@ int main(void)
 
     printf("Enter a date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &month, &day, &year);
-    printf("You entered the date %04d%02d%02d\n", year, month, day);
+    printf("You entered the date %4d%02d%02d\n", year, month, day);
 
     return 0;
 
---
+++ projects/chapter_03/product_info.c
@@ -0,0 +1,30 @@
+/* 
+Write a program that formats product information entered by the user. 
+A session with the program should look like this:
+Enter item number: 583
+Enter unit price: 13.5
+Enter purchase date (mm/dd/yyyy): 10/24/2010
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    float unit_price;
+    int item_number;
+    int day, month, year;
+
+    printf("Enter item number: "); 
+    scanf("%d", & item_number);
+
+    printf("Enter unit price: ");
+    scanf("%f", &unit_price);
+
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    printf("Item\tUnit\t\tPurchase\n\tPrice\t\tDate \n%-d\t$  %4.2f \b\t\b\b%4d/%02d/%02d\n", item_number, unit_price, month, day, year);
+
+    return 0;
+
+}
